e606363d082c67ebd8ffcc97e899f31e254a2bd4
Alex Novak <alexwnovak@gmail.com>
2016-12-12 13:27:42 -0500

    Fleshing out the pack command

--- JumpTool/JumpTool/Program.cs
+++ JumpTool/JumpTool/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace JumpTool
 {
@@ -30,6 +32,53 @@ namespace JumpTool
 
       private static void Pack()
       {
+         //string currentDirectory = Environment.CurrentDirectory;
+         string currentDirectory = @"C:\TF2\Maps\jump_lotus";
+
+         string mapName = Path.GetFileName( currentDirectory );
+         Console.WriteLine( $"Detected map name: {mapName}" );
+
+         // Get content
+
+         string contentDirectory = Path.Combine( currentDirectory, "Content" );
+
+         if ( !Directory.Exists( contentDirectory ) )
+         {
+            Console.WriteLine( "Content directory not found" );
+            Environment.Exit( 1 );
+         }
+
+         // Grok all content files
+
+         var allFiles = Directory.GetFiles( contentDirectory, "*", SearchOption.AllDirectories );
+
+         // Write a manifest
+
+         string fileList = Path.Combine( currentDirectory, mapName + "_files.txt" );
+
+         using ( var streamWriter = new StreamWriter( fileList ) )
+         {
+            foreach ( string file in allFiles )
+            {
+               string bspFile = file.Replace( contentDirectory, string.Empty ).Substring( 1 );
+
+               streamWriter.WriteLine( bspFile );
+               streamWriter.WriteLine( file );
+            }
+         }
+
+         // Pack
+
+         const string bspZipPath = @"C:\Program Files (x86)\Steam\steamapps\common\team fortress 2\bin\bspzip.exe";
+         string unpackedBspPath = Path.Combine( currentDirectory, mapName + ".bsp" );
+         string packedBspPath = Path.Combine( currentDirectory, "Output", mapName + ".bsp" );
+
+         var process = Process.Start( bspZipPath, $"-addorupdatelist {unpackedBspPath} {fileList} {packedBspPath}" );
+         process.WaitForExit();
+
+         // Delete the manifest
+
+         File.Delete( fileList );
       }
    }
 }
