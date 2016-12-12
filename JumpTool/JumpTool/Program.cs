9e3384fc8854d4d989906160a95298e98be464f6
Alex Novak <alexwnovak@gmail.com>
2016-12-12 12:52:00 -0500

    Now displays a syntax screen if arguments are omitted

--- JumpTool/JumpTool/Program.cs
+++ JumpTool/JumpTool/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace JumpTool
+﻿using System;
+
+namespace JumpTool
 {
    internal static class Program
    {
       private static void Main( string[] args )
       {
+         if ( args.Length == 0 )
+         {
+            Console.WriteLine( "Syntax: jump <command> <arguments>" );
+            Console.WriteLine();
+            Console.WriteLine( "Commands:" );
+            Console.WriteLine( "  pack        - Creates the file manifest and packs content into the .bsp" );
+            Environment.Exit( 1 );
+         }
       }
    }
 }
