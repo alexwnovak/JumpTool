3cc4e7d39a97bd205506bc460783828384ab92ca
Alex Novak <alexwnovak@gmail.com>
2017-01-02 18:13:55 -0500

    +"build" command help text

--- JumpTool/JumpTool/Program.cs
+++ JumpTool/JumpTool/Program.cs
@@ -13,6 +13,7 @@ namespace JumpTool
             Console.WriteLine( "Syntax: jump <command> <arguments>" );
             Console.WriteLine();
             Console.WriteLine( "Commands:" );
+            Console.WriteLine( "  build       - Compiles the map" );
             Console.WriteLine( "  pack        - Creates the file manifest and packs content into the .bsp" );
             Environment.Exit( 1 );
          }
